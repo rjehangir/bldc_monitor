a4964368b72478518b92b19e169d24882bdb16e9
Rustom Jehangir <rustyj110@gmail.com>
2013-12-09 10:21:13 -0800

    After getting a single basic plot to work, I added three other plots and labeled them for voltage, current, power, and rpm. This will eventually be done dynamically but it is hardcoded for now. The x axes are linked together so that time is the same on all plots. The auto panning function built into pyqtgraph was insufficient because it always centered on the data and we need right-aligned data so that the newest data is always showing. To accomplish this, the range is reset every time the window is redrawn so that it is constant width but always right-aligned with the newest time. The zooming functionality still works because the view width is recalculated each time. The y axis still uses the built in auto ranging functions.
    At this point, the serial connection needs to be tested and then the interface should be usable. There is still no way to save data to a file, but that should be fairly straightfoward.
    
    The original goal was to make the program dynamically reconfigurable with commands sent through serial. This goal will take some more work, particularly on the serial communication side. For now, the interface will be customized (hard-coded) for each particular case.

--- plotuino.py
+++ plotuino.py
@@ -14,7 +14,7 @@ class SerialConnection:
 	
 	# This function is temporary for testing purposes.
 	def readMessage(self):
-		return float(np.random.normal(size=1))
+		return np.random.normal(size=4)
 		
 	#def readMessage(self):
 		#preamble = ['\xFF','\xFA'];
@@ -27,17 +27,17 @@ class SerialConnection:
 				#input[0] = input[1]
 				#input[1] = ser.read(1)
 
-		## Get length
+		#Get length
 		#length = ser.read(1)
 
-		## Get data
+		#Get data
 		#data = ser.read(ord(length))
 
-		## Get checksum
+		#Get checksum
 		#input = ser.read(2)
 		#checksum = struct.unpack('H',input)[0]
 
-		## Verify checksum
+		#Verify checksum
 		#crc16 = crcmod.mkCrcFun(0x11021,0xFFFF,True)
 		#calcChecksum = crc16(data)
 		#calcChecksum = (~calcChecksum) % 2**16  # convert to uint16_t
@@ -46,11 +46,14 @@ class SerialConnection:
 			#print "Failed checksum."
 			#continue
 		      
-		## Break data into useful parts
+		#Break data into useful parts
 		#messageType = struct.unpack('H',data[0:2])[0]
-		#value = struct.unpack('f',data[2:6])[0]
+		#numValues = int(length/4)
+		#values = []
+		#for i in range(numValues):
+			#values.append(struct.unpack('f',data[4*i+2:4*i+6])[0])
 
-		#return value
+		#return values
 
 
 #######################################
@@ -65,26 +68,60 @@ win.setWindowTitle("Plotuino Test")
 # Enable antialiasing for prettier plots
 pg.setConfigOptions(antialias=True)
 
-p1 = win.addPlot(title="Test Plot")
-curve1 = p1.plot(pen='b')
-ydata = np.random.normal(size=0)
-xdata = ydata*0
-curve1.setData(xdata,ydata)
+plots = []
+curves = []
+timeData = np.ndarray(0)
+yData = [np.ndarray(0),np.ndarray(0),np.ndarray(0),np.ndarray(0)]
+
+plots.append(win.addPlot(title="Voltage"))
+plots.append(win.addPlot(title="Current"))
+win.nextRow()
+plots.append(win.addPlot(title="Power"))
+plots.append(win.addPlot(title="RPM"))
+
+plots[0].setLabel('left',"Volts",units='V')
+plots[1].setLabel('left',"Current",units='A')
+plots[2].setLabel('left',"Power",units='W')
+plots[3].setLabel('left',"RPM",units='RPM')
+
+colors = ['r','g','b','y']
+
+for i in range(len(plots)):
+	curves.append(plots[i].plot(pen=colors[i]))
+	
+	plots[i].enableAutoRange('y', True)
+	plots[i].enableAutoRange('x', False)
+	
+	plots[i].showGrid(x=True,y=True)
+	plots[i].setLabel('bottom',"Time",units='s')
+	
+plots[1].setXLink(plots[0])
+plots[2].setXLink(plots[0])
+plots[3].setXLink(plots[0])
 
 startTime = time.time()
 
 def update():
-	global xdata, ydata, curve1
-	value = sercon.readMessage()
-	ydata = np.append(ydata,value)
-	xdata = np.append(xdata,time.time()-startTime)
-	if ( len(ydata) > 1000 ):
-		xdata = np.delete(xdata,0,0)
-		ydata = np.delete(ydata,0,0)
+	global timeData,yData
+	values = sercon.readMessage()
+	for i in range(len(yData)):
+		yData[i] = np.append(yData[i],values[i])
+	timeData = np.append(timeData,time.time()-startTime)
+	if ( len(timeData) > 1000 ):
+		timeData = np.delete(timeData,0,0)
+		for i in range(len(yData)):
+			yData[i] = np.delete(yData[i],0,0)
 	
 def redraw():
-	global xdata, ydata, curve1
-	curve1.setData(xdata,ydata)
+	global timeData,yData,curves
+	for i in range(len(curves)):
+		curves[i].setData(timeData,yData[i])
+	for p in plots:
+		vr = p.viewRange()
+		xwidth = vr[0][1]-vr[0][0]
+		p.setXRange(timeData[len(timeData)-1]-xwidth,timeData[len(timeData)-1],padding=0,update=False)
+		p.enableAutoRange('y',True)
+		p.setAutoPan(x=True)
 	
 timer = QtCore.QTimer()
 timer.timeout.connect(update)
